e2f9d5c32385a8cd5dfc1e199daa226580bf0800
PriscilaRodriguez26 <prodriguez80843@ufide.ac.cr>
2024-10-22 21:17:40 -0600

    Finalización semana 06

--- src/main/java/com/tienda/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,4 +1,3 @@
-
 package com.tienda.tienda.service.impl;
 
 import com.tienda.tienda.dao.CategoriaDao;
@@ -7,28 +6,47 @@ import com.tienda.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author Priscilla Rodríguez
  */
 @Service
-public class CategoriaServiceImpl implements CategoriaService{
+public class CategoriaServiceImpl implements CategoriaService {
 
     @Autowired
     private CategoriaDao categoriaDao;
-    
+
     @Override
     public List<Categoria> getCategorias(boolean activos) {
         List<Categoria> lista = categoriaDao.findAll();
-        
+
         if (activos) {
             //remueve de la lista los elementos donde 
             //el atributo activo es false
             lista.removeIf(e -> !e.isActivo());
         }
-        
+
         return lista;
     }
-    
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
+
 }
