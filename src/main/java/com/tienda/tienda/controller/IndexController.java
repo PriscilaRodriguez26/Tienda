e298c1f4d870ca1d0c10a98e13c1bfa13ea8912b
PriscilaRodriguez26 <prodriguez80843@ufide.ac.cr>
2024-12-09 22:36:19 -0600

    Finalización semana 12

--- src/main/java/com/tienda/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/tienda/controller/IndexController.java
@@ -1,25 +1,44 @@
 
 package com.tienda.tienda.controller;
 
+import com.tienda.tienda.domain.Item;
+import com.tienda.tienda.service.ItemService;
+import com.tienda.tienda.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-/**
- *
- * @author Priscilla Rodríguez
- */
 @Controller
 public class IndexController {
     
-     @RequestMapping("/")
-    public String paginaInicio(Model model) {
-       // model.addAttribute("attribute", "value");
+    @Autowired
+    ProductoService productoService;
+    
+    @RequestMapping("/")
+    public String page(Model model) { 
+        var listaProductos = productoService.getProductos(true);
+        model.addAttribute("productos", listaProductos);
         return "index";
     }
-    @RequestMapping("/contacto")
-    public String paginaContacto(Model model) {
-       // model.addAttribute("attribute", "value");
-        return "info";
-    }
+	
+	@Autowired
+    private ItemService itemService;
+	
+	@RequestMapping("/refrescarBoton")
+    public ModelAndView refrescarBoton(Model model) { 
+        var lista = itemService.gets();
+        var totalCarritos = 0;
+        var carritoTotalVenta = 0;
+        for (Item i : lista) {
+            totalCarritos += i.getCantidad();
+            carritoTotalVenta += (i.getCantidad() * i.getPrecio());
+        }
+        model.addAttribute("listaItems", lista);
+        model.addAttribute("listaTotal", totalCarritos);
+        model.addAttribute("carritoTotal", carritoTotalVenta);
+        return new ModelAndView("/carrito/fragmentos :: verCarrito");
+    }    
 }
+
---
+++ src/main/java/com/tienda/tienda/dao/FacturaDao.java
@@ -0,0 +1,8 @@
+package com.tienda.tienda.dao;
+
+import com.tienda.tienda.domain.Factura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacturaDao extends JpaRepository<Factura, Long> {
+
+}
